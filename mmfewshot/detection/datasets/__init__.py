fe6e414df7cc8e3b53491a70c01c5363d435e761
Linyiqi <linyq29@gmail.com>
2021-09-01 22:46:14 +0800

    fix init (#28)

--- mmfewshot/detection/datasets/__init__.py
+++ mmfewshot/detection/datasets/__init__.py
@@ -5,13 +5,12 @@ from .dataset_wrappers import NwayKshotDataset, QueryAwareDataset
 from .few_shot_custom import FewShotCustomDataset
 from .pipelines import CropResizeInstance, GenerateMask
 from .utils import NumpyEncoder, query_support_collate_fn
-from .visualize import Visualizer
 from .voc import VOC_SPLIT, FewShotVOCDataset
 
 __all__ = [
     'build_dataloader', 'build_dataset', 'QueryAwareDataset',
     'NwayKshotDataset', 'NwayKshotDataloader', 'query_support_collate_fn',
     'FewShotCustomDataset', 'FewShotVOCDataset', 'FewShotCocoDataset',
-    'CropResizeInstance', 'GenerateMask', 'NumpyEncoder', 'Visualizer',
-    'COCO_SPLIT', 'VOC_SPLIT'
+    'CropResizeInstance', 'GenerateMask', 'NumpyEncoder', 'COCO_SPLIT',
+    'VOC_SPLIT'
 ]
